360017b9e54e095ad3bd193dc4c4ff4d4f4f3aa6
a-h-85 <39679638+a-h-85@users.noreply.github.com>
2018-05-29 10:39:14 +0200

    Add files via upload

---
+++ mvg_frankfurterring.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+curl 'https://www.mvg.de/fahrinfo/api/departure/750?footway=0' -H 'X-MVG-Authorization-Key: 5af1beca494712ed38d313714d4caff6' > /var/www/<<hostname>>/mvg_frankfurterring.json
